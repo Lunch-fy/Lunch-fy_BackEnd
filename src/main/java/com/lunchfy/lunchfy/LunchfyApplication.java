8508523c756fe4bd995e76ab29679666d6e24b15
NEROAT_73 <kikahi@naver.com>
2023-02-20 12:33:51 +0900

    First

---
+++ src/main/java/com/lunchfy/lunchfy/LunchfyApplication.java
@@ -0,0 +1,13 @@
+package com.lunchfy.lunchfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunchfy/lunchfy/LunchfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchfy.lunchfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
